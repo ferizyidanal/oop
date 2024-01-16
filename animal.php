08dbd8c2a1c9ffef9a197cb03ba190a80a153040
ferizyidanal <118708728+ferizyidanal@users.noreply.github.com>
2024-01-16 16:26:34 +0700

    animal.php

---
+++ animal.php
@@ -0,0 +1,15 @@
+<?php
+require_once "frog.php";
+    class Animal {
+        public $name;
+        public $legs;
+        public  $cold_blooded;
+     
+        public function __construct($name, $legs = 4, $cold_blooded= "no"){
+            $this-> name = $name;
+            $this-> legs = $legs;
+            $this-> cold_blooded = $cold_blooded;
+
+}
+}
+?>
